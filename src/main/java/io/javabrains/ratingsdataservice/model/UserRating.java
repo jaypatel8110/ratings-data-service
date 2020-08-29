8e84f9d898948ac404a16d82482d40e39838ff85
Jay Patel <jpatel8110@gmail.com>
2020-08-28 20:18:57 -0400

    All 3 Api calles

---
+++ src/main/java/io/javabrains/ratingsdataservice/model/UserRating.java
@@ -0,0 +1,29 @@
+package io.javabrains.ratingsdataservice.model;
+
+import io.javabrains.ratingsdataservice.model.Rating;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserRating {
+
+    private String userId;
+    private List<Rating> ratings;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+}
